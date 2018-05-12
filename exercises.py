5213416d5623d0dc7ef708519cf51cd8ed4ec75e
julian torres <juliannogueira@yahoo.com>
2018-05-12 01:02:04 -1000

    completion of exercise 8

--- exercises.py
+++ exercises.py
@@ -83,9 +83,12 @@ print(loving_it)
 #8
 #Function - check_product
 #Create a function named `check_product` which does not require any parameters.
+def check_product():
+    return sum * product
+
+print(check_product())
 
 #This function will multiply the values stored in the sum and product variables. Invoke this function and print your result.
-
 
 #9
 #Function - check_quotient
